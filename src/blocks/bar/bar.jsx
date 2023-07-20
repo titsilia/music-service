850a6d272e131a4d9bf3d49637029315cdfd8c27
elik <elik@example.com>
2023-07-20 20:46:34 +0500

    added lesson-6 files

--- src/blocks/bar/bar.jsx
+++ src/blocks/bar/bar.jsx
@@ -5,10 +5,11 @@ import styles from "./bar.module.css";
 import PlayerBlock from "./bar-components/player-block";
 
 function Bar() {
+
+
   return (
     <div className={styles.bar}>
       <div className={styles.bar__content}>
-        <div className={styles["bar__player-progress"]}></div>
         <PlayerBlock />
       </div>
     </div>
